8881bd50c178f484e2f5473be52a186e60b35e1b
ade-ski <ade_ski@live.com>
2016-05-22 23:14:14 -0700

    Skeleton for PLY lexer and parser for patterns

---
+++ regex_lexer.py
@@ -0,0 +1,57 @@
+import ply.lex as lex
+
+# Special characters
+special_chars = {
+    '.' : 'DOT',
+    '^' : 'CARET',
+    '$' : 'DOLLAR',
+    '*' : 'STAR',
+    '+' : 'PLUS',
+    '?' : 'QUESTION',
+    '(' : 'LPAREN',
+    ')' : 'RPAREN',
+    '{' : 'LCURLY',
+    '}' : 'RCURLY',
+    '[' : 'LSQUARE',
+    ']' : 'RSQUARE',
+    '<' : 'LANGLE',
+    '>' : 'RANGLE',
+    ',' : 'COMMA',
+    '-' : 'HYPHEN',
+    '|' : 'BAR',
+}
+
+# Escaped chars
+valid_escaped_chars = ['\\'] + list(special_chars.keys())
+
+# List of token names
+tokens = ['CHAR', 'DLANGLE', 'DRANGLE'] + list(special_chars.values())
+
+# Rules
+def t_DLANGLE(t):
+    r'<<'
+    return t
+
+def t_DRANGLE(t):
+    r'>>'
+    return t
+
+def t_escaped(t):
+    r'\\.'
+    if t.value[1] not in valid_escaped_chars:
+        raise Exception('Invalid escape character %s' % t)
+    t.type = 'CHAR'
+    t.value = t.value[1]
+    return t
+
+def t_CHAR(t):
+    r'.'
+    if t.value == '\\':
+        raise Exception('Dangling \\ at end of regular expression')
+    t.type = special_chars.get(t.value, 'CHAR')
+    return t
+
+def t_error(t):
+    raise Exception('Invalid input')
+    
+lexer = lex.lex()
---
+++ regex_parser.py
@@ -0,0 +1,44 @@
+import ply.yacc as yacc
+
+from regex_lexer import tokens
+
+def p_expression_alt(p):
+    'expression : alt'
+    p[0] = p[1]
+
+def p_alt_rec(p):
+    'alt : alt BAR concat'
+    p[0] = ('BAR', p[1], p[3])
+
+def p_alt_base(p):
+    'alt : concat'
+    p[0] = p[1]
+
+def p_concat_rec(p):
+    'concat : kstar concat'
+    p[0] = ('CONCAT', p[1], p[2])
+
+def p_concat_base(p):
+    'concat : kstar'
+    p[0] = p[1]
+
+def p_kstar_asterisk(p):
+    'kstar : atom STAR'
+    p[0] = ('KSTAR', p[1])
+
+def p_kstar_atom(p):
+    'kstar : atom'
+    p[0] = p[1]
+
+def p_atom_group(p):
+    'atom : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+def p_atom_char(p):
+    'atom : CHAR'
+    p[0] = p[1]
+
+def p_error(p):
+    print "Syntax error!"
+
+parser = yacc.yacc()
